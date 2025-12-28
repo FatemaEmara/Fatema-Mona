8a3baa17d73dc74620836c2cd84c4e9c363b48fa
monazarea <94869444+monazarea@users.noreply.github.com>
2025-12-28 12:16:05 +0200

    adding HelloWorld file

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+class HelloWorld{
+	void printHello(){
+		System.out.println("Hello World");
+	}
+	
+}
+class Main{
+	public static void main(String []args){
+		new HelloWorld().printHello();
+	}
+}
